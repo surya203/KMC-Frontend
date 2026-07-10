a4249496bf20954b50201df1103dc4df6b0ee5c5
ballsuresh <balla.suresh@logiqgen.com>
2026-07-10 11:37:24 +0530

    upadted my profile screen file

--- lib/features/dashboard/presentation/my_profile_screen.dart
+++ lib/features/dashboard/presentation/my_profile_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
